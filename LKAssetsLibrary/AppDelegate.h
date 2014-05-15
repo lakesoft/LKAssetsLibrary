a12bc2fbdd7afef0b996a4cbd5cfef664a0e9cca
Hiroshi Hashiguchi <xcatsan@mac.com>
2014-05-15 12:32:19 +0900

    Initial Commit

---
+++ LKAssetsLibrary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LKAssetsLibrary
+//
+//  Created by Hiroshi Hashiguchi on 2014/05/15.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LKAssetsLibrary/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LKAssetsLibrary
+//
+//  Created by Hiroshi Hashiguchi on 2014/05/15.
+//  Copyright (c) 2014年 lakesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
